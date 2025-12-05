765a9d248952161b0ab6b307f6f444bb36684683
Paolo Sasso <sasso@mac.com>
2025-12-05 10:59:04 +0000

    Day 5

--- src/y2025/day05/Aoc05.kt
+++ src/y2025/day05/Aoc05.kt
@@ -3,9 +3,7 @@ package y2025.day05
 import Day
 import kotlin.math.max
 
-private object Aoc05: Day() {
-
-}
+private object Aoc05: Day()
 
 fun main() {
 	Aoc05
@@ -23,8 +21,8 @@ fun main() {
 32"""
 		)
 		.initPart1(3, 888)
-		.inputAsText {
-			val (top, bottom) = it.split("\n\n")
+		.inputAsText { text ->
+			val (top, bottom) = text.split("\n\n")
 			val freshIDS = top.lines()
 				.map { line ->
 					val (a, b) = line.split("-")
@@ -38,25 +36,25 @@ fun main() {
 				}
 		}
 		.initPart2(14L, 344378119285354L)
-		.inputAsText {
-			val (top, _) = it.split("\n\n")
+		.inputAsText { text ->
+			val (top, _) = text.split("\n\n")
 			val freshIDS = top.lines()
 				.map { line ->
 					val (a, b) = line.split("-")
 					(a.toLong() .. b.toLong())
 				}
 				.sortedWith(
-					compareBy<LongRange> { it.start }
-						.thenBy { it.endInclusive }
+					compareBy<LongRange> { it.first }
+						.thenBy { it.last }
 				)
 
 			// ordered, now I have to look for overlaps
 			var nextIndex = -1L
 			var sum = 0L
-			freshIDS.forEach {
-				val currentStart = max(it.start, nextIndex)
-				if (it.endInclusive >= currentStart) {
-					nextIndex = it.endInclusive + 1
+			freshIDS.forEach { range ->
+				val currentStart = max(range.first, nextIndex)
+				if (range.last >= currentStart) {
+					nextIndex = range.last + 1
 					sum += (nextIndex - currentStart)
 				}
 			}
